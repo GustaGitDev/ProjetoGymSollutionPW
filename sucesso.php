1fa4da7972530ed7d3312dbc10effb8bb36ba99b
Gustavo <gutolights0@gmail.com>
2023-11-14 08:22:58 -0300

    exclusão de arquivo desnecessario

--- sucesso.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $nome = $_REQUEST["nome"];
-    $unidade = $_REQUEST["unidade"];
-    $mensagem = $_REQUEST["mensagem"];
-
-    echo "Obrigado, $nome. Mensagem recebida com sucesso!"
-
-?>
