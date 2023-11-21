d6dc1400d41f2d5e9be91e1b20613f4d2c60bfd1
Miguel <miguelgamesjua@gmail.com>
2023-11-20 22:32:17 -0300

    Criação do BD dinamicamente

--- CRUD/config.php
+++ CRUD/config.php
@@ -5,7 +5,14 @@
     define('BASE', 'cadastro');
 
     $conn = new MySQLi(HOST,USER,PASS,BASE);
+    mysqli_select_db($conn,'cadastro');
     
-
+    $query_create_table = "CREATE TABLE `usuarios` (
+        `id` int(10) UNSIGNED NOT NULL,
+        `nome` varchar(45) NOT NULL,
+        `email` varchar(255) NOT NULL,
+        `senha` varchar(255) NOT NULL,
+        `data_nasc` date NOT NULL
+      )";
 
 ?>
